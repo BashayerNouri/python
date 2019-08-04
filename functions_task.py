6a2a5206479ab64605116f3efdef3c31c972c4b7
Bashayer Nouri <bashayer_nouri@hotmail.com>
2019-08-04 18:03:35 +0300

    Age Calculator (Edited)

--- functions_task.py
+++ functions_task.py
@@ -6,30 +6,49 @@
 #             the program will give him back his age.
 
 
-from datetime import date
+# Tested: 21-12-1994 = You are 24 years, 7 months, and 13 days
+# Tested: 21-12-3000 = This birthdate is from the future! Please try another.
 
-today = date.today()
+
+
+from datetime import datetime
+
+today = datetime.now()
+
 
 def check_birthdate(year,month,day):
-  if(year >= today.year and month >= today.month and day > today.day):
-    print("This birthdate is from the future! Try another.")
-  else:
-    return calculate_age(year,month,day)
+
+	birthdate = datetime(year,month,day)
+
+	if(birthdate > today):
+		return False
+	else:
+		return True
+    
 
 
 def calculate_age(year,month,day):
-	if (month <= 12) and (day <= 31):
-		day = abs(today.day - day)
-		month = abs(today.month - month) 
-		year = abs(today.year - year)
-		print("You are " + str(year) +" years, " + str(month) + " months, and " + str(day) + " days")
-	else:
-		print("This birthdate is invalid! Try another.")
 
+	birthdate = datetime(year,month,day)
+
+	month_in_days = 30.4167 # one month = 30.4167 days
+	year_in_days = 365.2422 # ref: https://www.grc.nasa.gov/WWW/k-12/Numbers/Math/Mathematical_Thinking/calendar_calculations.htm
+
+	age = today - birthdate
+	age_in_days = age.days
+	age_in_years = int(age_in_days / year_in_days)
+	age_in_days = age_in_days % year_in_days
+	age_in_month = int(age_in_days / month_in_days)
+	age_in_days = round(age_in_days % month_in_days)
+
+	print("You are " + str(age_in_years) +" years, " + str(age_in_month) + " months, and " + str(age_in_days) + " days")
 
 
 year = int(input("Enter year of birth: "))
 month = int(input("Enter month of birth: "))
 day = int(input("Enter day of birth: "))
 
-check_birthdate(year,month,day)
+if check_birthdate(year,month,day):
+	calculate_age(year,month,day)
+else:
+	print("This birthdate is from the future! Please try another.")
